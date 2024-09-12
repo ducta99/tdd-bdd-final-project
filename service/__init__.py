18c6c0877153f6ca491febc4fb73a9f9de434ef7
ducta99 <ducbkhn99@gmail.com>
2024-09-12 17:45:01 +0700

    Finished assignment

--- service/__init__.py
+++ service/__init__.py
@@ -51,7 +51,7 @@ app.logger.info(70 * "*")
 try:
     models.init_db(app)  # make our sqlalchemy tables
 except Exception as error:  # pylint: disable=broad-except
-    app.logger.critical("%s: Cannot continue", error)
+    app.logger.critical(f"{error}: Cannot continue")  # Use string formatting here
     # gunicorn requires exit code 4 to stop spawning workers when they die
     sys.exit(4)
 
